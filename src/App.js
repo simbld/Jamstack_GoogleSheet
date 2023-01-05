f466d00fa5bc4de577e32c36321942ab2b096d85
SIMON BULLADO <s_bullado51@hotmail.com>
2023-01-05 15:59:57 +0100

    Install Papa Parse and response

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import papa from "papaparse";
 
 import "./App.css";
 
@@ -8,7 +9,8 @@ function App() {
 			"https://docs.google.com/spreadsheets/d/e/2PACX-1vSfzrIXvrQ-3p8rwErgELD88WKtZvb4d3ib_m8PQVf1ipvlPD03FmhbjQnDn57ULVaGTeu1IFNkU7ir/pub?output=csv"
 		)
 			.then((result) => result.text())
-			.then((text) => console.log(text));
+			.then((text) => papa.parse(text))
+			.then((data) => console.log(data));
 	}, []);
 	return (
 		<div className="App">
